9fa0ad369198feca6de55cf03fbea3c4196e6ffd
matthiasunt <mattu@post.com>
2020-03-04 18:53:07 +0100

    Try other scroll block

--- js/main.js
+++ js/main.js
@@ -8,15 +8,6 @@ var [lang, locale] = (((navigator.userLanguage || navigator.language).replace('-
 
 console.log(lang, locale);
 
-$(function() {
-    const $body = $(document);
-    $body.bind('scroll', function() {
-        if ($body.scrollLeft() !== 0) {
-            $body.scrollLeft(0);
-        }
-    });
-
-});
 
 // Example POST method implementation:
 async function postData(url = '', data = {}) {
